2c98f7f752fc59f70e9fdbedd524d941d3df628f
ClaudiaBiplus <claudia.filannino@gmail.com>
2023-12-21 15:10:26 +0000

    started js script with api url and key

---
+++ index.js
@@ -0,0 +1,6 @@
+var APIKey = "a37b207ab7356c57e7f33d87a8a3d1e1";
+
+var lat = x 
+var lon = y 
+
+var queryURL = "api.openweathermap.org/data/2.5/forecast?lat={" + lat + "}&lon={" + lon + "}&appid={" + APIKey + "}"
