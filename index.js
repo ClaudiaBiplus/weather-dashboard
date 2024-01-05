f779e5d9fc5bcadadcfd1cba236a4197a9cd0e33
ClaudiaBiplus <claudia.filannino@gmail.com>
2024-01-05 14:40:03 +0000

    got the average temperature for todays date

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ fetch(cityQueryURL)
       console.log(lat + ` ` + lon)
 
 //getting weather info
-      var coordQueryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey
+      var coordQueryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey + "&units=metric"
 
       fetch(coordQueryURL)
     .then(function(response) {
@@ -38,6 +38,58 @@ fetch(cityQueryURL)
     }).then(function(data) {
       console.log(data)
       console.log(data.list)
+      //getting date
+      //console.log(data.list[0].dt_txt)
+      const dateStr = data.list[0].dt_txt
+      console.log(dateStr)
+      const splitStr = dateStr.split(' ')
+      const date = splitStr[0]
+      console.log(date)
+      const compareDate = data.list[1].dt_txt.split(' ')[0]
+      console.log(compareDate)
+
+      //getting temperature
+      console.log(data.list[0].main.temp)
+      //getting wind
+      console.log(data.list[0].wind.speed)
+      //getting humidity
+      console.log(data.list[0].main.humidity)
+
+      //getting temperatures of the different days
+      let todayAllTemp = [data.list[0].main.temp];
+      let dayOneAllTemp = [];
+      let dayTwoAllTemp = [];
+      let dayThreeAllTemp =[];
+      let dayFourAllTemp = [];
+      let dayFiveAllTemp = [];
+      let fiveDaysAllTemp = []
+
+      console.log(data.list.length)
+
+      for (let j = 1; j < data.list.length; j++) {
+
+        if (data.list[0].dt_txt.split(' ')[0] === data.list[j].dt_txt.split(' ')[0]) {
+          todayAllTemp.push(data.list[j].main.temp)
+        } else {
+          fiveDaysAllTemp.push(data.list[j].main.temp)
+        }
+      }
+
+      function add(accumulator, currentValue) {
+        return accumulator + currentValue
+      }  
+      const todayAvrgTemp = Math.round(todayAllTemp.reduce(add) / todayAllTemp.length)
+
+      console.log(todayAllTemp)
+      console.log(todayAvrgTemp)
+
+
+
+
+
+
+
+
     })
 
     
